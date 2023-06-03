560c1025fb0a2004c809fa2a9b37fd4d477cb00b
DISSANG <dissangfrancis@yahoo.com>
2023-06-03 23:50:53 +0100

    Editer le message et afficher sa date de modification

--- message.js
+++ message.js
@@ -73,113 +73,175 @@ $(document).ready(function() {
   }
 
   // Fonction pour afficher les messages
-  function displayMessages(messages) {
-    var messagesContainer = $('#messages');
-    messagesContainer.empty();
-  
-    var getUserPromises = [];
-  
-    for (var i = messages.length - 1; i >= 0; i--) {
-      var message = messages[i];
-  
-      // Récupérer le pseudo de l'utilisateur
-      var getUserPromise = getUserPseudo(message.id_utilisateur);
-      getUserPromises.push(getUserPromise);
-  
-      (function(message) {
-        getUserPromise.then(function(pseudo) {
-          // Créer les éléments HTML pour afficher le message
-          var messageContainer = $('<div>').addClass('message-container').attr('data-message-id', message.id).attr('data-usermessage-id', message.id_utilisateur);;
-          var userElement = $('<div>').addClass('message-user').text(pseudo);
-          var contentElement = $('<div>').addClass('message-content').text(message.contenu);
-          var timestampElement = $('<div>').addClass('message-timestamp').text(message.date_creation);
-  
-          // Créer les icônes de modification et de suppression
-          var editIcon = $('<i>').addClass('fas fa-edit edit-icon');
-          var deleteIcon = $('<i>').addClass('fas fa-trash-alt delete-icon');
-  
-          // Ajouter les événements de clic pour les icônes
-          editIcon.click(function() {
-            // Action de modification du message
-           
-            // ...
-          });
-          deleteIcon.click(function() {
-            // Obtenir le conteneur du message parent
-            var messageContainer = $(this).closest('.message-container');
-          
-            // Obtenir l'identifiant du message à supprimer et vérifier si l'utilisateur a le droit de supprimer ce message
-            var messageId = messageContainer.data('message-id');
-            var usermessageId = messageContainer.data('usermessage-id');
-          
-            // Vérifier les conditions pour afficher le pop-up de confirmation
-            if (localStorage.getItem('userId') == usermessageId) {
-              // Afficher le pop-up de confirmation avec SweetAlert2
-              Swal.fire({
-                title: 'Confirmation de suppression',
-                text: 'Êtes-vous sûr de vouloir supprimer ce message ?',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Supprimer',
-                cancelButtonText: 'Annuler'
-              }).then(function(result) {
-                if (result.isConfirmed) {
-                  // Appeler la fonction pour supprimer le message
-                  deleteMessage(messageId, messageContainer);
-          
-                  // Afficher une alerte de suppression réussie avec SweetAlert2
-                  Swal.fire('Suppression réussie', 'Le message a été supprimé avec succès.', 'success');
-                }
-              });
-            } else {
-              Swal.fire('Accès refusé', 'Vous ne pouvez pas supprimer un message qui n\'est pas le vôtre dans le chat.', 'error');
+ // Fonction pour afficher les messages
+function displayMessages(messages) {
+  var messagesContainer = $('#messages');
+  messagesContainer.empty();
+
+  var getUserPromises = [];
+
+  for (var i = messages.length - 1; i >= 0; i--) {
+    var message = messages[i];
+
+    // Récupérer le pseudo de l'utilisateur
+    var getUserPromise = getUserPseudo(message.id_utilisateur);
+    getUserPromises.push(getUserPromise);
+
+    (function(message) {
+      getUserPromise.then(function(pseudo) {
+        // Créer les éléments HTML pour afficher le message
+        var messageContainer = $('<div>').addClass('message-container').attr('data-message-id', message.id).attr('data-usermessage-id', message.id_utilisateur);
+        var userElement = $('<div>').addClass('message-user').text(pseudo);
+        var contentElement = $('<div>').addClass('message-content').text(message.contenu);
+        var timestampElement = $('<div>').addClass('message-timestamp').text(message.date_creation);
+
+        // Créer les icônes de modification et de suppression
+        var editIcon = $('<i>').addClass('fas fa-edit edit-icon');
+        var deleteIcon = $('<i>').addClass('fas fa-trash-alt delete-icon');
+
+        // Ajouter les événements de clic pour les icônes
+        editIcon.click(function() {
+          // Obtenir le conteneur du message parent
+          var messageContainer = $(this).closest('.message-container');
+
+          // Obtenir l'identifiant du message à modifier
+          var messageId = messageContainer.data('message-id');
+
+          // Obtenir le contenu actuel du message
+          var currentContent = messageContainer.find('.message-content').text();
+
+          // Afficher le pop-up de modification avec SweetAlert2
+          Swal.fire({
+            title: 'Modifier le message',
+            html: '<input type="text" id="edit-message-input" class="swal2-input" value="' + currentContent + '">',
+            showCancelButton: true,
+            confirmButtonText: 'Enregistrer',
+            cancelButtonText: 'Annuler',
+            preConfirm: function() {
+              return $('#edit-message-input').val();
+            }
+          }).then(function(result) {
+            if (result.isConfirmed) {
+              // Obtenir le nouveau contenu du message depuis le pop-up
+              var newContent = result.value;
+
+              // Appeler la fonction pour mettre à jour le message
+              updateMessage(messageId, newContent);
             }
           });
-          
-          // Créer le conteneur des icônes
-          var iconsContainer = $('<div>').addClass('message-icons');
-          iconsContainer.append(editIcon, deleteIcon);
-  
-          // Ajouter les éléments au conteneur du message
-          var messageContentContainer = $('<div>').addClass('message-content-container');
-          messageContentContainer.append(contentElement, iconsContainer);
-          messageContainer.append(userElement, messageContentContainer, timestampElement);
-          messagesContainer.append(messageContainer);
-  
-          // Faire défiler jusqu'au bas du conteneur des messages
-          messagesContainer.scrollTop(messagesContainer.prop('scrollHeight'));
-        }).catch(function(error) {
-          console.error('Erreur lors de la récupération du pseudo de l\'utilisateur :', error);
         });
-      })(message);
-    }
-  
-    // Attendre que toutes les promesses de récupération du pseudo soient résolues
-    Promise.all(getUserPromises).catch(function(error) {
-      console.error('Erreur lors de la récupération des pseudos des utilisateurs :', error);
-    });
+
+        deleteIcon.click(function() {
+          // Obtenir le conteneur du message parent
+          var messageContainer = $(this).closest('.message-container');
+
+          // Obtenir l'identifiant du message à supprimer et vérifier si l'utilisateur a le droit de supprimer ce message
+          var messageId = messageContainer.data('message-id');
+          var usermessageId = messageContainer.data('usermessage-id');
+
+          // Vérifier les conditions pour afficher le pop-up de confirmation
+          if (localStorage.getItem('userId') == usermessageId) {
+            // Afficher le pop-up de confirmation avec SweetAlert2
+            Swal.fire({
+              title: 'Confirmation de suppression',
+              text: 'Êtes-vous sûr de vouloir supprimer ce message ?',
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonText: 'Supprimer',
+              cancelButtonText: 'Annuler'
+            }).then(function(result) {
+              if (result.isConfirmed) {
+                // Appeler la fonction pour supprimer le message
+                deleteMessage(messageId, messageContainer);
+
+                // Afficher une alerte de suppression réussie avec SweetAlert2
+                Swal.fire('Suppression réussie', 'Le message a été supprimé avec succès.', 'success');
+              }
+            });
+          } else {
+            Swal.fire('Accès refusé', 'Vous ne pouvez pas supprimer un message dans le chat qui n\'est pas le vôtre.', 'error');
+          }
+        });
+
+        // Créer le conteneur des icônes
+        var iconsContainer = $('<div>').addClass('message-icons');
+        iconsContainer.append(editIcon, deleteIcon);
+
+        // Ajouter les éléments au conteneur du message
+        var messageContentContainer = $('<div>').addClass('message-content-container');
+        messageContentContainer.append(contentElement, iconsContainer);
+        messageContainer.append(userElement, messageContentContainer, timestampElement);
+        messagesContainer.append(messageContainer);
+
+        // Faire défiler jusqu'au bas du conteneur des messages
+        messagesContainer.scrollTop(messagesContainer.prop('scrollHeight'));
+      }).catch(function(error) {
+        console.error('Erreur lors de la récupération du pseudo de l\'utilisateur :', error);
+      });
+    })(message);
   }
-  function deleteMessage(messageId, messageContainer) {
-    // Faire une requête AJAX pour supprimer le message en utilisant l'identifiant
-    $.ajax({
-      url: 'controller/Messagecontroller.php',
-      dataType: 'json',
-      type: 'DELETE', // Utiliser la méthode DELETE
-      data: { messageId: messageId },
-      success: function(response) {
-        if (response.success) {
-          // Supprimer le conteneur du message de l'interface
-          messageContainer.remove();
-        } else {
-          console.error('Erreur lors de la suppression du message :', response.message);
-        }
-      },
-      error: function(error) {
-        console.error('Erreur lors de la suppression du message :', error);
+
+  // Attendre que toutes les promesses de récupération du pseudo soient résolues
+  Promise.all(getUserPromises).catch(function(error) {
+    console.error('Erreur lors de la récupération des pseudos des utilisateurs :', error);
+  });
+}
+
+function deleteMessage(messageId, messageContainer) {
+  // Faire une requête AJAX pour supprimer le message en utilisant l'identifiant
+  $.ajax({
+    url: 'controller/Messagecontroller.php',
+    dataType: 'json',
+    type: 'DELETE', // Utiliser la méthode DELETE
+    data: { messageId: messageId },
+    success: function(response) {
+      if (response.success) {
+        // Supprimer le conteneur du message de l'interface
+        messageContainer.remove();
+      } else {
+        console.error('Erreur lors de la suppression du message :', response.message);
       }
-    });
-  }
-  
+    },
+    error: function(error) {
+      console.error('Erreur lors de la suppression du message :', error);
+    }
+  });
+}
+
+function updateMessage(messageId, newContent) {
+  // Faire une requête AJAX pour mettre à jour le message en utilisant l'identifiant et le nouveau contenu
+  $.ajax({
+    url: 'controller/Messagecontroller.php',
+    dataType: 'json',
+    type: 'PUT', // Utiliser la méthode PUT
+    data: {
+      id: messageId,
+      contenu: newContent
+    },
+    success: function(response) {
+      if (response.success) {
+        // Mettre à jour le contenu du message dans l'interface
+        var messageContainer = $('.message-container[data-message-id="' + messageId + '"]');
+        messageContainer.find('.message-content').text(newContent);
+
+        // Mettre à jour la date du message dans l'interface
+        var timestampElement = messageContainer.find('.message-timestamp');
+        var currentDate = new Date();
+        var formattedDate = currentDate.toLocaleString(); // ou utilisez un format de date approprié
+        timestampElement.text(formattedDate);
+
+        // Afficher une alerte de mise à jour réussie avec SweetAlert2
+        Swal.fire('Mise à jour réussie', 'Le message a été modifié avec succès.', 'success');
+      } else {
+        console.error('Erreur lors de la mise à jour du message :', response.message);
+      }
+    },
+    error: function(error) {
+      console.error('Erreur lors de la mise à jour du message :', error);
+    }
+  });
+}
+
 
   // Fonction pour récupérer les messages depuis le serveur
   function getMessages(chatId) {
